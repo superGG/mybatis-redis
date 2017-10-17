1a8e5eea35007a77958386fae01757ef852fa0e3
Kellan_Song <459714770@qq.com>
2017-10-17 16:16:18 +0800

    修改user对象信息

--- src/main/java/com/kellan/entity/User.java
+++ src/main/java/com/kellan/entity/User.java
@@ -9,6 +9,10 @@ public class User implements Serializable{
 	
 	private int id;
 	private String name;
+	private String password;
+	private String image;
+	private String createTime;
+	
 	public int getId() {
 		return id;
 	}
@@ -21,8 +25,27 @@ public class User implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + "]";
+		return "User [id=" + id + ", name=" + name + ", password=" + password
+				+ ", image=" + image + ", createTime=" + createTime + "]";
 	}
 }
